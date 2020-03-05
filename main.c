a05ffde656c598346cf57c6c3e09917fd62a2702
David Flores Ruiz <ie717807@iteso.mx>
2020-03-04 19:59:04 -0600

    2 tareas create

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * Copyright 2016-2020 NXP
  * All rights reserved.
  */
- 
+
 /*
  * @file:			AlarmaFreeRTOS.c
  * @brief:   		Tarea3_ALARMA_in_FreeRTOS_OS_for_FRDM-K66F
@@ -19,6 +19,7 @@
 #include "clock_config.h"
 #include "MK66F18.h"
 #include "fsl_debug_console.h"
+
 /* TODO: insert other include files here. */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -26,6 +27,7 @@
 #include "Tareas.h"
 #include "semphr.h"
 #include "event_groups.h"
+
 /* TODO: insert other definitions and declarations here. */
 
 typedef struct
@@ -40,6 +42,7 @@ typedef struct
 const char *pcTextForSeconds1 = "Task 1 is running\r\n";
 const char *pcTextForMinutes2 = "Task 2 is running\r\n";
 const char *pcTextForHours3   = "Task 3 is running\r\n";
+
 int main(void) {
 
   	/* Init board hardware. */
@@ -50,12 +53,13 @@ int main(void) {
     BOARD_InitDebugConsole();
 
     PRINTF("Hello World\n");
+
     /* Before a semaphore is used it must be explicitly created.  In this
-   	example	a binary semaphore is created. */
-    static parameters_task_t parameters_task;
-    parameters_task.semaphore_FreeRTOs = xSemaphoreCreateBinary();
-    parameters_task.event_FreeRtos     = xEventGroupCreate();
-    parameters_task.mutex_UART_freertos= xSemaphoreCreateMutex();
+	 example a binary semaphore is created. */
+	static parameters_task_t parameters_task;
+	parameters_task.semaphore_FreeRTOs = xSemaphoreCreateBinary();
+	parameters_task.event_FreeRtos = xEventGroupCreate();
+	parameters_task.mutex_UART_freertos = xSemaphoreCreateMutex();
 
 	/* Create the first task at priority 1... */
 	/* xTaskCreate uses:
@@ -67,19 +71,28 @@ int main(void) {
 	 * 		We are not using the task handle.
 	 * 		*/
 
-	xTaskCreate(task_Seconds, "task_Seconds", 500, (void*)&parameters_task, configMAX_PRIORITIES,
-			NULL);
-	xTaskCreate(task_Minutes, "task_Minutes", 500, (void*)&parameters_task, configMAX_PRIORITIES,
-			NULL);
-	xTaskCreate(task_Hours, "task_Hours", 500, (void*)&parameters_task, configMAX_PRIORITIES,
-			NULL);
+	xTaskCreate(task_Seconds, "task_Seconds", 500, (void*) &parameters_task,
+			configMAX_PRIORITIES, NULL);
+
+	xTaskCreate(task_Minutes, "task_Minutes", 500, (void*) &parameters_task,
+			configMAX_PRIORITIES, NULL);
+
+	xTaskCreate(task_Hours, "task_Hours", 	  500, (void*) &parameters_task,
+			configMAX_PRIORITIES, NULL);
+
+	xTaskCreate(task_Alarm, "task_Alarm", 	  500, (void*) &parameters_task,
+			configMAX_PRIORITIES, NULL);
+
+	xTaskCreate(task_Print, "task_Alarm", 	  500, (void*) &parameters_task,
+			configMAX_PRIORITIES, NULL);
+
 
 	/* Start the scheduler so the created tasks start executing. */
 	vTaskStartScheduler();
 
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-
-    }
+	/* Enter an infinite loop, just incrementing a counter. */
+	while (1) {
+		// Never arrive here!
+	}
     return 0 ;
 }

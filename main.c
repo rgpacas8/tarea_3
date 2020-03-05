eac8dd817c5dc9b39f9373751614bad7480ac278
rgpacas8 <ie706818@iteso.mx>
2020-03-04 18:58:48 -0600

    Parametros Tasks

--- main.c
+++ main.c
@@ -24,12 +24,19 @@
 #include "task.h"
 #include "queue.h"
 #include "Tareas.h"
+#include "semphr.h"
+#include "event_groups.h"
 /* TODO: insert other definitions and declarations here. */
-/*
-static void task_Seconds( void *pvParameters );
-static void task_Minutes( void *pvParameters );
-static void task_Hours  ( void *pvParameters );
-*/
+
+typedef struct
+{
+	SemaphoreHandle_t semaphore_FreeRTOs;
+	EventGroupHandle_t event_FreeRtos;
+	QueueHandle_t queue_freertos;
+	SemaphoreHandle_t mutex_UART_freertos;
+
+} parameters_task_t;
+
 const char *pcTextForSeconds1 = "Task 1 is running\r\n";
 const char *pcTextForMinutes2 = "Task 2 is running\r\n";
 const char *pcTextForHours3   = "Task 3 is running\r\n";
@@ -43,20 +50,32 @@ int main(void) {
     BOARD_InitDebugConsole();
 
     PRINTF("Hello World\n");
+    /* Before a semaphore is used it must be explicitly created.  In this
+   	example	a binary semaphore is created. */
+    static parameters_task_t parameters_task;
+    parameters_task.semaphore_FreeRTOs = xSemaphoreCreateBinary();
+    parameters_task.event_FreeRtos     = xEventGroupCreate();
+    parameters_task.mutex_UART_freertos= xSemaphoreCreateMutex();
 
-    /* Create the first task at priority 1... */
-    /* xTaskCreate uses:
-     * 		Pointer to the function that implements the task.
-     * 		Text name for the task.  This is to facilitate debugging only.
-     * 		Stack depth - most small microcontrollers will use much less stack than this.
-     * 		Task parameter, Pasamos una cadena de texto para DEBUG
-     * 		Priority
-     * 		We are not using the task handle.
-     * 		*/
+	/* Create the first task at priority 1... */
+	/* xTaskCreate uses:
+	 * 		Pointer to the function that implements the task.
+	 * 		Text name for the task.  This is to facilitate debugging only.
+	 * 		Stack depth - most small microcontrollers will use much less stack than this.
+	 * 		Task parameter, Pasamos una cadena de texto para DEBUG
+	 * 		Priority
+	 * 		We are not using the task handle.
+	 * 		*/
 
-    xTaskCreate( task_Seconds, "task_Seconds", 500, (void*)pcTextForSeconds1, 1, NULL );
-    xTaskCreate( task_Minutes, "task_Minutes", 500, (void*)pcTextForMinutes2, 1, NULL );
-    xTaskCreate( task_Hours  , "task_Hours"  , 500, (void*)pcTextForHours3, 1, NULL );
+	xTaskCreate(task_Seconds, "task_Seconds", 500, (void*)&parameters_task, configMAX_PRIORITIES,
+			NULL);
+	xTaskCreate(task_Minutes, "task_Minutes", 500, (void*)&parameters_task, configMAX_PRIORITIES,
+			NULL);
+	xTaskCreate(task_Hours, "task_Hours", 500, (void*)&parameters_task, configMAX_PRIORITIES,
+			NULL);
+
+	/* Start the scheduler so the created tasks start executing. */
+	vTaskStartScheduler();
 
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
